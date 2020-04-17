d6922e5da5fec8c038d5ceadaef6e5e9015d4972
erickpoleto <erick-poleto@hotmail.com>
2020-04-17 20:29:15 -0300

    frontend

--- src/modules/token.js
+++ src/modules/token.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth') 
+require("dotenv").config()
 
 module.exports = {
         token(params = {}) {
-            return jwt.sign(params, authConfig.secret, {
+            return jwt.sign(params, process.env.AUTH, {
             expiresIn: 86400,
         })
     }
